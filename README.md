baf868ed432b3a1fc3684ed65720cb3151099912
Lars Norberg <github@larsnorbergofficial.com>
2022-08-10 16:28:20 +0200

    rewrite to fully automatic

--- README.md
+++ README.md
@@ -3,12 +3,8 @@
 [![discord](https://www.goldpawsstuff.com/shared/img/common/dd-button.png)](https://discord.gg/MUSfWXd)
 [![twitter](https://www.goldpawsstuff.com/shared/img/common/tw-button.png)](https://twitter.com/GoldpawsStuff)
 
-This is an addon for those that hate fun.
+This is an addon for those that hate involuntary fun on their expense. It automatically removes known transform effects like masks, turkey transforms and so on by removing its associated aura.
 
-It gives you a clickable red X when you are under the effect of a known Hallow's Eve or Blingtron dance transform. Clicking the X removes the transform and makes you beautiful again.
-
-You can move the button by holding down Ctrl and dragging it. And you can reset its position by holding down Shift and clicking it. Note that this can only be done when the button is actually visible, and I don't intend to change this behavior.
+Note that auras can only be removed in this manner outside of combat. If you are transformed while engaged in combat, the aura will be removed once combat ends.
 
 ***\*If you're transformed by an aura not currently on the list, please report the name of the aura that transformed you using the issues link above, and I'll add it in the next update!***
-
-***\*\*Also note that since this addon uses secure macro buttons to be able to remove auras, it can only parse and update the button outside of combat. This means if somebody wands you during combat, the button will not appear until combat ends. This is a game limitation.***
--- Unmasked/main.lua
+++ Unmasked/main.lua
@@ -23,241 +23,152 @@
 	SOFTWARE.
 
 --]]
-local ADDON = ...
+local Addon,Private = ...
 
-local Unmasked = _G.UnmaskedActionButton
-if (not Unmasked) then
-	return
-end
+-- Lua API
+local pairs = pairs
 
 -- WoW API
+local CancelSpellByName = CancelSpellByName
+local GetSpellInfo = GetSpellInfo
 local InCombatLockdown = InCombatLockdown
-local UnitAura = UnitAura
+local UnitBuff = UnitBuff
 
-local auras = {
-
-	-- Blingtron
-	[134522] = true, 	-- Dressed to Kill (Male?)
-	[222256] = true, 	-- Dressed to Kill (Female?)
-
-	-- Toys
-	[354546] = true, 	-- Inner Child -- VERIFIED!
-	[354909] = true, 	-- Inner Child
-	[354910] = true, 	-- Inner Child
-	[354911] = true, 	-- Inner Child
-	[354912] = true, 	-- Inner Child
-	[354913] = true, 	-- Inner Child
-	[354914] = true, 	-- Inner Child
-	[354915] = true, 	-- Inner Child
-	[354916] = true, 	-- Inner Child
-	[354917] = true, 	-- Inner Child
-	[354919] = true, 	-- Inner Child
-	[354920] = true, 	-- Inner Child
-	[354922] = true, 	-- Inner Child
-	[354924] = true, 	-- Inner Child
-	[354940] = true, 	-- Inner Child
-	[354941] = true, 	-- Inner Child
-	[354942] = true, 	-- Inner Child
-	[354943] = true, 	-- Inner Child
-	[354944] = true, 	-- Inner Child
-	[354945] = true, 	-- Inner Child
-	[354946] = true, 	-- Inner Child
-	[354947] = true, 	-- Inner Child
-	[354948] = true, 	-- Inner Child
-	[354949] = true, 	-- Inner Child
-	[354950] = true, 	-- Inner Child
-	[354951] = true, 	-- Inner Child
-	[354952] = true, 	-- Inner Child
-	[354953] = true, 	-- Inner Child
-	[354954] = true, 	-- Inner Child
-	[354955] = true, 	-- Inner Child
-	[354956] = true, 	-- Inner Child
-	[354957] = true, 	-- Inner Child
-	[354958] = true, 	-- Inner Child
-	[354959] = true, 	-- Inner Child
-	[354960] = true, 	-- Inner Child
-	[354961] = true, 	-- Inner Child
-	[354962] = true, 	-- Inner Child
-
-	-- Hallow's End Costumes
-	[172010] = true, 	-- Abomination Costume
-	[218132] = true, 	-- Banshee Costume
-	[ 24732] = true, 	-- Bat Costume
-	[191703] = true, 	-- Bat Costume
-	[285521] = true, 	-- Blue Dragon Body Costume
-	[285519] = true, 	-- Blue Dragon Head Costume
-	[285523] = true, 	-- Blue Dragon Tail Costume
-	[ 97135] = true, 	-- Children's Costume Aura
-	[257204] = true, 	-- Dirty Horse Costume
-	[257205] = true, 	-- Dirty Horse Costume
-	[191194] = true, 	-- Exquisite Deathwing Costume
-	[192472] = true, 	-- Exquisite Deathwing Costume
-	[217917] = true, 	-- Exquisite Grommash Costume
-	[171958] = true, 	-- Exquisite Lich King Costume
-	[190837] = true, 	-- Exquisite VanCleef Costume
-	[246237] = true, 	-- Exquisite Xavius Costume
-	[191210] = true, 	-- Gargoyle Costume
-	[172015] = true, 	-- Geist Costume
-	[ 24735] = true, 	-- Ghost Costume
-	[ 24736] = true, 	-- Ghost Costume
-	[191700] = true, 	-- Ghost Costume
-	[191698] = true, 	-- Ghost Costume
-	[172008] = true, 	-- Ghoul Costume
-	[190826] = true, 	-- Gnoll Costume (17th Anniversary)
-	[190824] = true, 	-- Murloc Costume (17th Anniversary)
-	[243319] = true, 	-- Quilboar Costume (17th Anniversary)
-	[243317] = true, 	-- Trogg Costume (17th Anniversary)
-	[285522] = true, 	-- Green Dragon Body Costume
-	[285520] = true, 	-- Green Dragon Head Costume
-	[285524] = true, 	-- Green Dragon Tail Costume
-	[246242] = true, 	-- Horse Head Costume
-	[246241] = true, 	-- Horse Tail Costume
-	[ 44212] = true, 	-- Jack-o'-Lanterned!
-	[177656] = true, 	-- Kor'kron Foot Soldier Costume
-	[177657] = true, 	-- Kor'kron Foot Soldier Costume
-	[ 24712] = true, 	-- Leper Gnome Costume
-	[ 24713] = true, 	-- Leper Gnome Costume
-	[191701] = true, 	-- Leper Gnome Costume
-	[171479] = true, 	-- "Lil' Starlet" Costume
-	[171470] = true, 	-- "Mad Alchemist" Costume
-	[191211] = true, 	-- Nerubian Costume
-	[ 24710] = true, 	-- Ninja Costume
-	[ 24711] = true, 	-- Ninja Costume
-	[191686] = true, 	-- Ninja Costume
-	[334407] = true, 	-- Noble Bearing (The Harvest - Revendreth Treasure)
-	[ 24708] = true, 	-- Pirate Costume
-	[173958] = true, 	-- Pirate Costume
-	[173959] = true, 	-- Pirate Costume
-	[191682] = true, 	-- Pirate Costume
-	[191683] = true, 	-- Pirate Costume
-	[ 61716] = true, 	-- Rabbit Costume
-	[233598] = true, 	-- Red Dragon Body Costume
-	[233594] = true, 	-- Red Dragon Head Costume
-	[233599] = true, 	-- Red Dragon Tail Costume
-	[ 30167] = true, 	-- Red Ogre Costume
-	[102362] = true, 	-- Red Ogre Mage Costume
-	[ 24723] = true, 	-- Skeleton Costume
-	[191702] = true, 	-- Skeleton Costume
-	[172003] = true, 	-- Slime Costume
-	[172020] = true, 	-- Spider Costume
-	[ 99976] = true, 	-- Squashling Costume
-	[243321] = true, 	-- Tranquil Mechanical Yeti Costume
-	[ 61781] = true, 	-- Turkey Feathers
-	[178306] = true, 	-- Warsong Orc Costume
-	[178307] = true, 	-- Warsong Orc Costume
-	[191208] = true, 	-- Wight Costume
-	[ 24740] = true, 	-- Wisp Costume
-	[279509] = true, 	-- Witch!
-	[171930] = true, 	-- "Yipp-Saron" Costume
-
-	--[   768] = true 	-- Cat Form (DEVELOPMENT PURPOSES ONLY!)
-}
-
--- Return a value rounded to the nearest integer.
-local math_round = function(value, precision)
-	if (precision) then
-		value = value * 10^precision
-		value = (value + .5) - (value + .5)%1
-		value = value / 10^precision
-		return value
-	else 
-		return (value + .5) - (value + .5)%1
-	end 
-end
-
--- Convert a coordinate within a frame to a usable position
-local parse = function(parentWidth, parentHeight, x, y, bottomOffset, leftOffset, topOffset, rightOffset)
-	if (y < parentHeight * 1/3) then 
-		if (x < parentWidth * 1/3) then 
-			return "BOTTOMLEFT", leftOffset, bottomOffset
-		elseif (x > parentWidth * 2/3) then 
-			return "BOTTOMRIGHT", rightOffset, bottomOffset
-		else 
-			return "BOTTOM", x - parentWidth/2, bottomOffset
-		end 
-	elseif (y > parentHeight * 2/3) then 
-		if (x < parentWidth * 1/3) then 
-			return "TOPLEFT", leftOffset, topOffset
-		elseif x > parentWidth * 2/3 then 
-			return "TOPRIGHT", rightOffset, topOffset
-		else 
-			return "TOP", x - parentWidth/2, topOffset
-		end 
-	else 
-		if (x < parentWidth * 1/3) then 
-			return "LEFT", leftOffset, y - parentHeight/2
-		elseif (x > parentWidth * 2/3) then 
-			return "RIGHT", rightOffset, y - parentHeight/2
-		else 
-			return "CENTER", x - parentWidth/2, y - parentHeight/2
-		end 
-	end 
-end
-
--- Needed for position parsing. Can probably simplify a bit.
-local getParsedPosition = function(self)
-
-	-- Retrieve UI coordinates
-	local uiScale = UIParent:GetEffectiveScale()
-	local uiWidth, uiHeight = UIParent:GetSize()
-	local uiBottom = UIParent:GetBottom()
-	local uiLeft = UIParent:GetLeft()
-	local uiTop = UIParent:GetTop()
-	local uiRight = UIParent:GetRight()
-
-	local worldWidth = math_round(WorldFrame:GetWidth())
-	local worldHeight = math_round(WorldFrame:GetHeight()) -- this is 768, always. why not just assume?
-
-	-- Turn UI coordinates into unscaled screen coordinates
-	uiWidth = uiWidth*uiScale
-	uiHeight = uiHeight*uiScale
-	uiBottom = uiBottom*uiScale
-	uiLeft = uiLeft*uiScale
-	uiTop = uiTop*uiScale - worldHeight -- use values relative to edges, not origin
-	uiRight = uiRight*uiScale - worldWidth -- use values relative to edges, not origin
-
-	-- Retrieve frame coordinates
-	local frameScale = self:GetEffectiveScale()
-	local x, y = self:GetCenter()
-	local bottom = self:GetBottom()
-	local left = self:GetLeft()
-	local top = self:GetTop()
-	local right = self:GetRight()
-
-	-- Turn frame coordinates into unscaled screen coordinates
-	x = x*frameScale
-	y = y*frameScale
-	bottom = bottom*frameScale
-	left = left*frameScale
-	top = top*frameScale - worldHeight -- use values relative to edges, not origin
-	right = right*frameScale - worldWidth -- use values relative to edges, not origin
-
-	-- Figure out the frame position relative to the UI master frame
-	left = left - uiLeft
-	bottom = bottom - uiBottom
-	right = right - uiRight
-	top = top - uiTop
-
-	-- Figure out the point within the given coordinate space
-	local point, offsetX, offsetY = parse(uiWidth, uiHeight, x, y, bottom, left, top, right)
-
-	-- Convert coordinates to the frame's scale. 
-	return point, offsetX/frameScale, offsetY/frameScale
-end
-
-Unmasked.GetParsedPosition = getParsedPosition
-Unmasked:SetScript("OnHide", function(self) self.macro = nil end) -- trigger a re-iteration on next show
-Unmasked:SetScript("OnEvent", function(self, event, ...) 
+Private.frame = CreateFrame("Frame", nil, WorldFrame)
+Private.frame:RegisterEvent("ADDON_LOADED")
+Private.frame:SetScript("OnEvent", function(self, event, ...)
 
 	if (event == "ADDON_LOADED") then
-		local addon = ...;
-		if (addon == "Unmasked") then
-			local db = Unmasked_DB
-			if (db) and (db.point) and (db.offsetX) and (db.offsetX) then
-				self:ClearAllPoints()
-				self:SetPoint(db.point, db.offsetX, db.offsetY)
-			end
+		local addon = ...
+		if (addon == Addon) then
 			self:UnregisterEvent("ADDON_LOADED")
+			self.auras = {}
+			for id in pairs({
+
+				-- Blingtron
+				[134522] = true, 	-- Dressed to Kill (Male?)
+				[222256] = true, 	-- Dressed to Kill (Female?)
+
+				-- Toys
+				[354546] = true, 	-- Inner Child -- VERIFIED!
+				[354909] = true, 	-- Inner Child
+				[354910] = true, 	-- Inner Child
+				[354911] = true, 	-- Inner Child
+				[354912] = true, 	-- Inner Child
+				[354913] = true, 	-- Inner Child
+				[354914] = true, 	-- Inner Child
+				[354915] = true, 	-- Inner Child
+				[354916] = true, 	-- Inner Child
+				[354917] = true, 	-- Inner Child
+				[354919] = true, 	-- Inner Child
+				[354920] = true, 	-- Inner Child
+				[354922] = true, 	-- Inner Child
+				[354924] = true, 	-- Inner Child
+				[354940] = true, 	-- Inner Child
+				[354941] = true, 	-- Inner Child
+				[354942] = true, 	-- Inner Child
+				[354943] = true, 	-- Inner Child
+				[354944] = true, 	-- Inner Child
+				[354945] = true, 	-- Inner Child
+				[354946] = true, 	-- Inner Child
+				[354947] = true, 	-- Inner Child
+				[354948] = true, 	-- Inner Child
+				[354949] = true, 	-- Inner Child
+				[354950] = true, 	-- Inner Child
+				[354951] = true, 	-- Inner Child
+				[354952] = true, 	-- Inner Child
+				[354953] = true, 	-- Inner Child
+				[354954] = true, 	-- Inner Child
+				[354955] = true, 	-- Inner Child
+				[354956] = true, 	-- Inner Child
+				[354957] = true, 	-- Inner Child
+				[354958] = true, 	-- Inner Child
+				[354959] = true, 	-- Inner Child
+				[354960] = true, 	-- Inner Child
+				[354961] = true, 	-- Inner Child
+				[354962] = true, 	-- Inner Child
+
+				-- Hallow's End Costumes
+				[172010] = true, 	-- Abomination Costume
+				[218132] = true, 	-- Banshee Costume
+				[ 24732] = true, 	-- Bat Costume
+				[191703] = true, 	-- Bat Costume
+				[285521] = true, 	-- Blue Dragon Body Costume
+				[285519] = true, 	-- Blue Dragon Head Costume
+				[285523] = true, 	-- Blue Dragon Tail Costume
+				[ 97135] = true, 	-- Children's Costume Aura
+				[261477] = true, 	-- Dervish
+				[257204] = true, 	-- Dirty Horse Costume
+				[257205] = true, 	-- Dirty Horse Costume
+				[191194] = true, 	-- Exquisite Deathwing Costume
+				[192472] = true, 	-- Exquisite Deathwing Costume
+				[217917] = true, 	-- Exquisite Grommash Costume
+				[171958] = true, 	-- Exquisite Lich King Costume
+				[190837] = true, 	-- Exquisite VanCleef Costume
+				[246237] = true, 	-- Exquisite Xavius Costume
+				[191210] = true, 	-- Gargoyle Costume
+				[172015] = true, 	-- Geist Costume
+				[ 24735] = true, 	-- Ghost Costume
+				[ 24736] = true, 	-- Ghost Costume
+				[191700] = true, 	-- Ghost Costume
+				[191698] = true, 	-- Ghost Costume
+				[172008] = true, 	-- Ghoul Costume
+				[190826] = true, 	-- Gnoll Costume (17th Anniversary)
+				[285522] = true, 	-- Green Dragon Body Costume
+				[285520] = true, 	-- Green Dragon Head Costume
+				[285524] = true, 	-- Green Dragon Tail Costume
+				[246242] = true, 	-- Horse Head Costume
+				[246241] = true, 	-- Horse Tail Costume
+				[ 44212] = true, 	-- Jack-o'-Lanterned!
+				[177656] = true, 	-- Kor'kron Foot Soldier Costume
+				[177657] = true, 	-- Kor'kron Foot Soldier Costume
+				[ 24712] = true, 	-- Leper Gnome Costume
+				[ 24713] = true, 	-- Leper Gnome Costume
+				[191701] = true, 	-- Leper Gnome Costume
+				[171479] = true, 	-- "Lil' Starlet" Costume
+				[171470] = true, 	-- "Mad Alchemist" Costume
+				[ 58493] = true, 	-- Mohawked!
+				[190824] = true, 	-- Murloc Costume (17th Anniversary)
+				[191211] = true, 	-- Nerubian Costume
+				[ 24710] = true, 	-- Ninja Costume
+				[ 24711] = true, 	-- Ninja Costume
+				[191686] = true, 	-- Ninja Costume
+				[334407] = true, 	-- Noble Bearing (The Harvest - Revendreth Treasure)
+				[ 61734] = true, 	-- Noblegarden Bunny
+				[ 24708] = true, 	-- Pirate Costume
+				[173958] = true, 	-- Pirate Costume
+				[173959] = true, 	-- Pirate Costume
+				[191682] = true, 	-- Pirate Costume
+				[191683] = true, 	-- Pirate Costume
+				[243319] = true, 	-- Quilboar Costume (17th Anniversary)
+				[ 61716] = true, 	-- Rabbit Costume
+				[233598] = true, 	-- Red Dragon Body Costume
+				[233594] = true, 	-- Red Dragon Head Costume
+				[233599] = true, 	-- Red Dragon Tail Costume
+				[ 30167] = true, 	-- Red Ogre Costume
+				[102362] = true, 	-- Red Ogre Mage Costume
+				[ 24723] = true, 	-- Skeleton Costume
+				[191702] = true, 	-- Skeleton Costume
+				[172003] = true, 	-- Slime Costume
+				[172020] = true, 	-- Spider Costume
+				[ 99976] = true, 	-- Squashling Costume
+				[243321] = true, 	-- Tranquil Mechanical Yeti Costume
+				[243317] = true, 	-- Trogg Costume (17th Anniversary)
+				[ 61781] = true, 	-- Turkey Feathers
+				[178306] = true, 	-- Warsong Orc Costume
+				[178307] = true, 	-- Warsong Orc Costume
+				[191208] = true, 	-- Wight Costume
+				[ 24740] = true, 	-- Wisp Costume
+				[279509] = true, 	-- Witch!
+				[171930] = true, 	-- "Yipp-Saron" Costume
+
+				--[   768] = true 	-- Cat Form (DEVELOPMENT PURPOSES ONLY!)
+			}) do
+				local name = GetSpellInfo(id)
+				if (name) then
+					self.auras[name] = true
+				end
+			end
 		else
 			return
 		end
@@ -268,41 +179,16 @@ Unmasked:SetScript("OnEvent", function(self, event, ...)
 	elseif (event == "PLAYER_REGEN_ENABLED") then
 		self:UnregisterEvent("PLAYER_REGEN_ENABLED")
 	end
-	local macro
 
-	-- Iterate current auras
 	local index = 1
-	local name, _, _, _, _, _, _, _, _, spellID = UnitAura("player", index, "HELPFUL")
-	while (spellID) do
-		if (auras[spellID]) then
-			if (macro) then
-				macro = macro .."\n".."/cancelaura "..name
-			else
-				macro = "/cancelaura "..name
-			end
+	while true do
+		local name = UnitBuff("player", index)
+		if (not name) then
+			return
+		end
+		if (self.auras[name]) then
+			CancelSpellByName(name)
 		end
 		index = index + 1
-		name, _, _, _, _, _, _, _, _, spellID = UnitAura("player", index, "HELPFUL")
 	end
-
-	if (macro) then
-		if (macro ~= self.macro) then
-			self.macro = macro
-			self:SetAttribute("macrotext", macro)
-			self:Show()
-		end
-	elseif (self.macro) then
-		self.macro = nil
-		self:SetAttribute("macrotext")
-		self:Hide()
-	end
-
 end)
-
--- Clear the macro and hide the button securely on clicks.
-SecureHandlerWrapScript(Unmasked, "PostClick", Unmasked, [[ self:Hide(); self:SetAttribute("macrotext", nil); ]])
-
-Unmasked.bg = Unmasked:CreateTexture()
-Unmasked.bg:SetAllPoints()
-Unmasked.bg:SetTexture([[Interface\AddOns\]]..ADDON..[[\icons-raid-targets.tga]])
-Unmasked.bg:SetTexCoord(2/4, 3/4, 1/4, 2/4)
